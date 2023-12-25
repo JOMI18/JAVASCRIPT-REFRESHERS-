403904dd2d297c885a4de70ba9dbd3ee3dfcaf4d
Oluwajomiloju <getjommy@gmail.com>
2023-12-25 21:03:42 +0100

    Fundamentals-1-Completed

---
+++ Fundamentals-Part-1/Challenges/challenge2.js
@@ -0,0 +1,106 @@
+// CHALLENGE #3
+// There are two gymnastics teams: Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins a trophy!
+
+// Your tasks:
+
+// 1. Calculate the average score for each team, using the test data included below. The average score for Dolphins should be assigned to the scoreDolphins variable, and the average score of Koalas should be assigned to the scoreKoalas variable.
+
+// 2. Compare the team's average scores to determine the winner of the competition, and print to the console:
+
+// "Dolphins win the trophy" if Dolphins win, or
+
+// "Koalas win the trophy" if Koalas win, or
+
+// "Both win the trophy" if their average scores are equal.
+
+// TEST DATA: Dolphins scored 96, 108, and 89. Koalas scored 88, 91, and 110.
+
+// // 3. BONUS 1: Include a requirement for a minimum score of 100. With this CSSPageRule, a team  only wins if it has a higher score than the other team, and at the same time has a score of at least 100 points
+
+// HINT: Use a logical operator to test for minimum score, as well as multiple if else blocks
+// TEST DATA BONUS 1: Dolphins scored 97, 112, and 89. Koalas scored 109, 95, and 123.
+
+// 4. BONUS 2: Minimum score also applies to a draw! So a draW only happened when both teams have the same score and both have a score greater or equal to 100 ProcessingInstruction.apply. Otherwise no team wins
+
+// TEST DATA: Dolphins scored 97, 112, and 101. Koalas scored 109, 95, and 106.
+
+let scoreDolphins = (96 + 108 + 89) / 3;
+let scoreKoalas = (88 + 91 + 110) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas) {
+  console.log("Dolphins win the trophy");
+} else if (scoreDolphins < scoreKoalas) {
+  console.log("Koalas win the trophy");
+} else if (scoreDolphins === scoreKoalas) {
+  console.log("Both win the trophy");
+}
+
+// BONUS 1
+
+scoreDolphins = (97 + 112 + 101) / 3;
+scoreKoalas = (109 + 95 + 123) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+  console.log("Dolphins win the trophy");
+} else if (scoreDolphins < scoreKoalas && scoreKoalas >= 100) {
+  console.log("Koalas win the trophy");
+} else if (scoreDolphins === scoreKoalas) {
+  console.log("Both win the trophy");
+}
+
+// BONUS 2
+
+scoreDolphins = (97 + 112 + 101) / 3;
+scoreKoalas = (109 + 95 + 106) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+  console.log("Dolphins win the trophy");
+} else if (scoreDolphins < scoreKoalas && scoreKoalas >= 100) {
+  console.log("Koalas win the trophy");
+} else if (
+  scoreDolphins === scoreKoalas &&
+  scoreKoalas >= 100 &&
+  scoreDolphins >= 100
+) {
+  console.log("Both win the trophy");
+} else {
+  console.log("No one wins");
+}
+
+// CHALLENGE #4
+// Steven needs a very simple tip calculator for whenever he goes to eat in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+
+// Your tasks:
+
+// Calculate the tip, depending on the bill value. Create a variable called tip for this. It's not allowed to use an if...else statement (if it's easier for you, you can start with an if...else statement, and then try to convert it to a ternary operator).
+
+// Print a string to the console containing the bill value, the tip, and the final value (bill + tip).
+
+// Example: The bill was 275, the tip was 41.25, and the total value 316.25.
+
+// Note: Use the values of the bill and tip variables to construct this string. Don't hard-code them 🙂
+
+// TEST DATA: Test with different bill values: 275, 40, and 430
+
+// HINT: To calculate 20% of a value, simply multiply it by 20/100 = 0.2
+
+// HINT 2: Value X is between 50 and 300, if it's >= 50 && <= 300 😉
+
+const bill = 275;
+let tip;
+
+if (bill >= 50 && bill <= 300) {
+  tip = 0.15 * bill;
+} else {
+  tip = 0.2 * bill;
+}
+console.log(tip);
+
+tip = bill >= 50 && bill <= 300 ? 0.15 * bill : 0.2 * bill;
+console.log(tip);
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
+);
