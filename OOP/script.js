733b8fe9e4e5eec1a878b2fffacd76a52db8324e
Oluwajomiloju <getjommy@gmail.com>
2024-03-26 14:53:51 +0100

    understanding oop

--- OOP/script.js
+++ OOP/script.js
@@ -1,5 +1,10 @@
 "use strict";
 
+// 3 ways of implementing oop in js
+// constructor functionc
+// es6 classes
+
+// object.create
 ///////////////////////////////////////
 // Constructor Functions and the new Operator
 
@@ -308,3 +313,264 @@ console.log(mike instanceof Object);
 
 Student.prototype.constructor = Student; // you need to set it manually, because object.create made it such that person was the prototype
 console.dir(Student.prototype.constructor);
+
+///////////////////////////////////////
+// Inheritance Between "Classes": ES6 Classes
+
+class StudentCls extends PersonCl {
+  // constructor(fullName, birthYear, course) {
+  //   // super is the constructor function of the personCl class
+  //   // Always needs to happen first
+  //   super(fullName, birthYear);
+  //   this.course = course;
+  // }
+}
+
+const martha = new StudentCls("Martha Jones", 2012);
+// But anyway, this was just to demonstrate to you  that if you do not need any new properties,  then you don't even need  to bother writing a constructor method in the child class.
+
+console.log(martha);
+class StudentCl extends PersonCl {
+  constructor(fullName, birthYear, course) {
+    // super is the constructor function of the personCl class
+    // Always needs to happen first
+    super(fullName, birthYear);
+
+    this.course = course;
+  }
+  introduce() {
+    console.log(`My name is ${this.fullName} and I study ${this.course}`);
+  }
+
+  // to override a method in the parent
+  calcAge() {
+    console.log(
+      `I'm ${
+        2037 - this.birthYear
+      } years old, but as a student I feel more like ${
+        2037 - this.birthYear + 10
+      }`
+    );
+  }
+}
+const mariah = new StudentCl("Mariah Jones", 2012, "Computer Science");
+console.log(mariah);
+mariah.introduce();
+mariah.calcAge();
+
+///////////////////////////////////////
+// Inheritance Between "Classes": Object.create
+
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  // BUG in video:
+  // console.log(`My name is ${this.fullName} and I study ${this.course}`);
+
+  // FIX:
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const jazz = Object.create(StudentProto);
+jazz.init("Jazz", 2010, "Computer Science");
+jazz.introduce();
+jazz.calcAges();
+console.log(jazz);
+
+///////////////////////////////////////
+//SHOWS NEED FOR Encapsulation:
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+
+    this.movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+}
+const acc1 = new Account("Jonas", "USD", 1111);
+console.log(acc1);
+
+// this isnt the best way to do it
+acc1.movements.push(250);
+acc1.movements.push(-140);
+// So, instead of interacting with a property like this, it's a lot better to create methods that interact with these properties. And that is especially true for important properties, such as these movements here. So, this will for sure avoid bugs in the future, as your application grows.
+
+console.log(acc1);
+
+class UsedAccount {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+
+    this.movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+    }
+  }
+
+  // Public interface
+  deposit(val) {
+    this.movements.push(val);
+  }
+
+  withdraw(val) {
+    // Also this withdraw method here actually abstracts the fact  that a withdrawal is basically a negative movement.  So here as we did it manually,  we needed to pass in minus 140.  But now, as we do a withdrawal, it's of course,  a lot more natural to write
+    // that simply 140 are gonna be withdrawn.  So, what I'm trying to say is that this minus here,  is something that the user of this object,  shouldn't be caring about.  And so now, we actually abstracted that away,
+    // right into this object, and in particular into this withdrawal method. So, just a very small obstruction here in this case, but it's still worth to notice.
+    this.deposit(-val);
+  }
+}
+
+const acc2 = new UsedAccount("Jonathan", "EUR", 1111);
+
+acc2.deposit(250);
+acc2.withdraw(140);
+acc2.approveLoan(1000); // shouldnt be able to do this
+acc2.requestLoan(1000);
+
+console.log(acc2);
+
+console.log(acc2.pin);
+// And the same goes, for example, for the pin.  So, of course, we can access the pin  from outside of the account.  So, you see, but probably it shouldn't be accessible  from outside of the class, but yeah,  of course, right now it is accessible.  And this is actually a very real and very important concern.
+
+///////////////////////////////////////
+// Encapsulation: Protected Properties and Methods
+// Now first, remember that encapsulation basically means to keep some properties and methods private inside the class so that they are not accessible from outside of the class. Then the rest of the methods are basically exposed as a public interface, which we can also call API.
+// So this is essential to do in anything more than a toy application.
+// Now, there are two big reasons why we need encapsulation and data privacy.
+// So first it is to prevent code  from outside of a class to accidentally manipulate  or data inside the class.  And that's exactly what we did  by the end of the last lecture here.  So this is also the reason why  we implement a public interface.  So we are not supposed to manually mess with this property  and therefore we should encapsulate it.
+// Now, the second reason is that when we expose only a small interface so a small API consisting only of a few public methods then we can change all the other internal methods with more confidence. Because in this case, we can be sure that external code does not rely on these private methods. And so therefore our code will not break when we do internal changes.
+
+// Encapsulation: Private Class Fields and Methods
+// Now for starters, why is this proposal actually called Class fields? Well, in traditional OOP languages like Java and C++, properties are usually called fields. So what this means is that with this new proposal, JavaScript is moving away from the idea that classes are just syntactic sugar over constructor functions.
+// Because with this new class features classes actually start to have abilities that we didn't previously have with constructor functions. Now to many developers consider this to be a big problem but personally, I'm not sure if it is such a big deal for the average JavaScript developer.
+// So as long as you still understand how prototypal inheritance and function constructors work then I believe that you will be fine.
+
+// 1) Public fields
+// 2) Private fields
+// 3) Public methods
+// 4) Private methods
+// (there is also the static version)
+
+class EncapsAccount {
+  // Fields will are on the instances
+  // 1) Public fields (instances)
+  locale = navigator.language;
+  // _movements = [];
+
+  // 2) Private fields (instances)
+  #movements = [];
+  #pin;
+
+  constructor(owner, currency, pin) {
+    // fields cannot be in constructors
+
+    this.owner = owner;
+    this.currency = currency;
+
+    this.#pin = pin;
+
+    // doesn't make it truly private- its just a convention
+    // Protected property
+    // this._pin = pin;
+    // this._movements = [];
+    // this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // 3) Public methods
+
+  // Public interface
+  // methods will be added on the prototype
+  getMovements() {
+    // return this._movements;
+    return this.#movements;
+  }
+
+  deposit(val) {
+    // this._movements.push(val);
+
+    this.#movements.push(val);
+    return this;
+  }
+
+  withdraw(val) {
+    // Also this withdraw method here actually abstracts the fact  that a withdrawal is basically a negative movement.  So here as we did it manually,  we needed to pass in minus 140.  But now, as we do a withdrawal, it's of course,  a lot more natural to write
+    // that simply 140 are gonna be withdrawn.  So, what I'm trying to say is that this minus here,  is something that the user of this object,  shouldn't be caring about.  And so now, we actually abstracted that away,
+    // right into this object, and in particular into this withdrawal method. So, just a very small obstruction here in this case, but it's still worth to notice.
+
+    this.deposit(-val);
+    return this;
+  }
+
+  // _approveLoan(val) {
+  //   return true;
+  // }
+
+  requestLoan(val) {
+    // if (this._approveLoan(val)) {
+    if (this.#approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+      return this;
+    }
+  }
+
+  static helper() {
+    // not available on all instances just on the class
+    console.log("Helper");
+  }
+
+  // 4) Private methods
+
+  //  And private methods, as we already mentioned earlier  are very useful to hide the implementation details  from the outside.
+  #approveLoan(val) {
+    return true;
+  }
+}
+
+const jomi = new EncapsAccount("Jomi", "USD", 1234);
+
+// (_) brings awareness to dev that its not meant to be used
+// jomi._movements.push(250);
+// jomi._movements.push(-140);
+
+
+jomi.deposit(250);
+jomi.withdraw(140);
+jomi.requestLoan(1000);
+console.log(jomi.getMovements()); // without it being overridden(it being set)
+
+// proof its now private
+// console.log(jomi.#movements);
+// console.log(jomi.#pin);
+// console.log(jomi.#approveLoan(100));
+
+console.log(jomi);
+EncapsAccount.helper();
+
+// Chaining--wont work until each method returns this
+
+jomi.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
+console.log(jomi.getMovements());
